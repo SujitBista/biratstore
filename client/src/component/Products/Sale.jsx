2a6bd34ce87401041ec21059c0fa4b1b9b9aff94
Sujit Bista <sujitbistaprogrammer@gmail.com>
2024-03-14 11:16:07 +0545

    added print feature to a cart and formatted a code for visibity

--- client/src/component/Products/Sale.jsx
+++ client/src/component/Products/Sale.jsx
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styles from './Sale.module.css';
+import '../../print-styles.css';
 
 function Sale() {
     const [filteredProducts, setFilteredProducts] = useState([]);
@@ -14,14 +15,14 @@ function Sale() {
 
     useEffect(() => {
         const fetchData = async () => {
-            try{
-            const response = await axios.get('http://localhost:3001/product/liquors');
-            const result = response.data;
-             setProducts(result);
-            } catch(error) {
+            try {
+                const response = await axios.get('http://localhost:3001/product/liquors');
+                const result = response.data;
+                setProducts(result);
+            } catch (error) {
                 console.log(error);
             }
-        }
+        };
         fetchData();
     }, [refresh]);
 
@@ -29,74 +30,74 @@ function Sale() {
         setErrorMessage('');
         const searchTerm = event.target.value.toLowerCase();
         setSearchTerm(searchTerm);
-        if(searchTerm) {
-        const filteredOptions = products.filter((product) =>
-          product.product_name.toLowerCase().includes(searchTerm)
-        );
-        setFilteredProducts(filteredOptions);
-        setSelectedProductId('');
-        }else {
+        if (searchTerm) {
+            const filteredOptions = products.filter((product) =>
+                product.product_name.toLowerCase().includes(searchTerm)
+            );
+            setFilteredProducts(filteredOptions);
+            setSelectedProductId('');
+        } else {
             setFilteredProducts([]);
         }
-      };
+    };
 
     const handleClick = (product_id, product_name) => {
         setSelectedProductId(product_id);
         setSearchTerm(product_name);
         setFilteredProducts([]);
-    }
+    };
 
     const handleQuantityChange = (event) => {
         setQuantity(parseInt(event.target.value));
     };
 
-    const handleAddToCart = (event,productId) => {
+    const handleAddToCart = (event, productId) => {
         let updatedItem;
         let sameProductFound = false;
         event.preventDefault();
         const id = parseInt(productId);
         //from the list of products find the specific item that is matched
-        let item = products.find(product => product.product_id === id);
-        if(quantity > item.qty) {
-            setErrorMessage('Qty you entered exceeds the qty you are trying to sell')
+        let item = products.find((product) => product.product_id === id);
+        if (quantity > item.qty) {
+            setErrorMessage('Qty you entered exceeds the qty in stock');
             return;
         }
-        
+
         const updatedCartItems = addToCart.map((cartProduct) => {
-            if(item.product_id === cartProduct.product_id) {
+            if (item.product_id === cartProduct.product_id) {
                 sameProductFound = true;
-                if(cartProduct.qty + quantity > item.qty) {
-                    setErrorMessage('Qty you entered exceeds the qty you are trying to sell');
+                if (cartProduct.qty + quantity > item.qty) {
+                    setErrorMessage('Qty you entered exceeds the qty in stock');
                     return cartProduct;
                 }
-                return {...item, qty: cartProduct.qty + quantity, updatedPrice: item.price * (cartProduct.qty + quantity)}
+                return { ...item, qty: cartProduct.qty + quantity, updatedPrice: item.price * (cartProduct.qty + quantity) };
             }
             return cartProduct;
-        })
-        if(sameProductFound) {
+        });
+        if (sameProductFound) {
             setAddToCart(updatedCartItems);
         } else {
-            updatedItem = {...item, updatedPrice: item.price * quantity, qty: quantity};
+            updatedItem = { ...item, updatedPrice: item.price * quantity, qty: quantity };
             setAddToCart([...addToCart, updatedItem]);
-        } 
-    }
-    
+        }
+    };
+
     const calculateTotalPrice = () => {
         let total = 0;
-        addToCart.forEach(item => total+= parseInt(item.updatedPrice));
+        addToCart.forEach((item) => (total += parseInt(item.updatedPrice)));
         return total;
-    }
+    };
 
     const handleCheckOut = async () => {
         try {
             const patchRequest = addToCart.map((item) => {
-                const matchedProduct = products.find(product => product.product_id === item.product_id);
-                if(matchedProduct) {
-                    const updatedQty =  matchedProduct.qty - item.qty;
+                const matchedProduct = products.find((product) => product.product_id === item.product_id);
+                if (matchedProduct) {
+                    const updatedQty = matchedProduct.qty - item.qty;
                     return axios.patch('http://localhost:3001/api/checkout', {
                         product_id: matchedProduct.product_id,
                         unit: item.unit,
-                        qty: updatedQty
+                        qty: updatedQty,
                     });
                 } else {
                     console.log(`Product with ID ${item.product_id} not found.`);
@@ -106,87 +107,90 @@ function Sale() {
             await Promise.all(patchRequest);
             setAddToCart([]);
             setRefresh(!refresh);
-        } catch(error) {
+        } catch (error) {
             console.log(error);
         }
-    }
-
+    };
+    const handlePrintTable = () => {
+        window.print();
+    };
+    
     return (
         <>
             {errorMesssage && <div className={styles.errorMsg}>{errorMesssage}</div>}
             <h1>Add Sale</h1>
             <div>
                 <div className={styles.productSearch}>
-                    <input
-                        type="text"
-                        placeholder="Product Search"
-                        value={searchTerm}
-                        onChange={handleSearch}
-                    />
+                    <input type="text" placeholder="Product Search" value={searchTerm} onChange={handleSearch} />
                 </div>
 
-                {filteredProducts.length > 0 && <div className={styles.searchList}>
-                    {filteredProducts.map((filteredProduct, index) => (
-                        <div key={index} onClick={() => handleClick(filteredProduct.product_id, filteredProduct.product_name)}>{filteredProduct.product_name}</div>
-                    ))}
-                </div>
-                }
-            </div>  
+                {filteredProducts.length > 0 && (
+                    <div className={styles.searchList}>
+                        {filteredProducts.map((filteredProduct, index) => (
+                            <div key={index} onClick={() => handleClick(filteredProduct.product_id, filteredProduct.product_name)}>
+                                {filteredProduct.product_name}
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
             {selectedProductId && (
                 <form onSubmit={(event) => handleAddToCart(event, selectedProductId)}>
                     <div className={styles.quantity}>
                         <label htmlFor="quantity">Quantity:</label>
-                        <input
-                            type="number"
-                            id="quantity"
-                            name="quantity"
-                            value={quantity}
-                            onChange={handleQuantityChange}
-                        />
+                        <input type="number" id="quantity" name="quantity" value={quantity} onChange={handleQuantityChange} />
                     </div>
-                    <button className={styles.button} type="submit">Add to Cart</button>
+                    <button className={styles.button} type="submit">
+                        Add to Cart
+                    </button>
                 </form>
             )}
-            {addToCart.length > 0 && <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Qty</th>
-                        <th>Unit Price</th>
-                        <th>Price</th>
-                        <th>Unit</th>
-                    </tr>
-                </thead>
-                <tbody>
-                 {addToCart.map(cart => (
-                    <tr key={cart.product_id}>
-                        <td>{cart.product_name}</td>
-                        <td>{cart.qty}</td>
-                        <td>{cart.price}</td>
-                        <td>{cart.updatedPrice}</td>
-                        <td>{cart.units}</td>
-                    </tr>
-                 ))}
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>{calculateTotalPrice() !== 0 ? `Total: ${calculateTotalPrice()}`: ''}</td>
-                        <td></td>
-                    </tr>
-                    <tr></tr>
-                   <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td><button onClick={handleCheckOut} style={{background: 'green', color: 'white', borderRadius: '25px'}}>Buy Now</button></td>
-                    </tr>
-                </tbody>
-            </table>
-            }
+
+            {addToCart.length > 0 && (
+                <table id="myTable">
+                    <thead style={{ background: 'green' }}>
+                        <tr>
+                            <th>Name</th>
+                            <th>Qty</th>
+                            <th>Unit Price</th>
+                            <th>Price</th>
+                            <th>Unit</th>
+                            <th className="hideDuringPrint">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody style={{ backgroundColor: 'gold' }}>
+                        {addToCart.map((cart) => (
+                            <tr key={cart.product_id}>
+                                <td>{cart.product_name}</td>
+                                <td>{cart.qty}</td>
+                                <td>{cart.price}</td>
+                                <td>{cart.updatedPrice}</td>
+                                <td>{cart.units}</td>
+                                <td className="hideDuringPrint"></td>
+                            </tr>
+                        ))}
+
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td style={{ fontSize: '35px' }}>Grand Total:</td>
+                            <td style={{ fontSize: '30px' }}>{calculateTotalPrice() !== 0 ? ` ${calculateTotalPrice()}` : ''}</td>
+                            <td>
+                                <button className={`${styles.button} hideDuringPrint`} onClick={handleCheckOut} style={{ background: 'green', color: 'white', borderRadius: '25px' }}>
+                                    Buy Now
+                                </button>
+                            </td>
+                            <td>
+                                <button className="hideDuringPrint" onClick={handlePrintTable}>
+                                    Print
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            )}
         </>
-    )
+    );
 }
 
 export default Sale;
